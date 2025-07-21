f89b471298418b9553c9626f6745440827c22558
Zhenlong Zhang <zzl010824@gmail.com>
2025-07-21 10:29:03 +0800

    Update c3_allModel2_V_QDiff.m

--- c3_allModel2_V_QDiff.m
+++ c3_allModel2_V_QDiff.m
@@ -68,9 +68,7 @@ function rlddm_fit_fixed_params_by_section_fminunc
             realParams = [ ...
                 10 / (1 + exp(-safeLog(bestParams(1)))), ...  % k  (0,10)
                 10 / (1 + exp(-safeLog(bestParams(2)))), ...  % a  (0,10)
-                1  / (1 + exp(-safeLog(bestParams(3)))), ...  % T  (0,1)
-                bestParams(4)                               % w 
-            ];
+                1  / (1 + exp(-safeLog(bestParams(3))))];
  
  
             all_results(end+1, :) = {ratID, sectionIDs(s), n_trials, ...
